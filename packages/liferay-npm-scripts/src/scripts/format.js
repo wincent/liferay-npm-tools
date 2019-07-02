6bbf133ccc2d738866a1d0a0403eba50e70a61ad
Greg Hurrell <greg.hurrell@liferay.com>
2019-07-02 18:18:45 +0200

    feat: handling globbing internally
    TODO: write proper message here
    
    will cover:
    
    - ESLint using one glob engine
    - Prettier has own engine
    - want to isolate from those differences
    - will switch to programmatic invocation of tools (to avoid command line
      arg length limits)
    - will be able to produce quieter output for CI
    - should be faster due to shortcircuiting
    - and provides a model for how we could use prettier inside csf to
    format JS in jsp

--- packages/liferay-npm-scripts/src/scripts/format.js
+++ packages/liferay-npm-scripts/src/scripts/format.js
@@ -45,6 +45,45 @@ function format(options = {}) {
 		return;
 	}
 
+	console.log('testing');
+	const preprocessGlob = require('../utils/preprocessGlob');
+	const expandGlobs = require('../utils/expandGlobs');
+	const IGNORES = require('../utils/readIgnoreFile')('.prettierignore');
+	// have to add node_modules because prettier does tt by default
+	IGNORES.unshift('node_modules/**');
+	console.log(IGNORES);
+	const g = [];
+	console.log('preproc');
+	globs.forEach(gl => g.push(...preprocessGlob(gl)));
+	console.log(g);
+	const start = Date.now();
+	const results = expandGlobs(g, IGNORES);
+	console.log('got', results.length, 'in', Date.now() - start, 'ms');
+	// got 1442 in 2230 ms
+	// visited count 81907
+	// without pruning:
+	// got 1442 in 2117 ms (high variance between runs)
+	// visited count 80215
+
+	const prettier = require('prettier');
+	const opts = getMergedConfig('prettier');
+	console.log('opts', opts);
+	// fuller test...
+	const ok = results.map(r => {
+		// console.log(r);
+		try {
+			return prettier.check(fs.readFileSync(r).toString(), {...opts, filepath: r})
+		} catch (e) {
+			console.log('caught');
+			return false
+		}
+	});
+	console.log('at end', Date.now() - start, ok.filter(Boolean).length);
+	// got 35k files... in 3771ms --- nope, needed to ignore node_modules
+	// checked them in: about 35s, claims 1407 good out of 1473 checked
+	// some may be syntax erros (the catch)... there were 3 of those
+	return;
+
 	const CONFIG_PATH = path.join(process.cwd(), 'TEMP-prettier-config.json');
 
 	fs.writeFileSync(CONFIG_PATH, JSON.stringify(getMergedConfig('prettier')));
